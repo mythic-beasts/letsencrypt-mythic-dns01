5715c6b5432ea4e61253709be1871659cde6c3de
Toby Goodwin <toby@paccrat.org>
2018-07-19 13:07:25 +0100

    need to pass args to group_args, also tweak nl definition

--- dehydrated-mythic-dns01.sh
+++ dehydrated-mythic-dns01.sh
@@ -3,9 +3,7 @@
 # Copyright (c) 2018 Mythic Beasts Ltd
 
 group_args() {
-    local nl
-    nl='
-'
+    local nl=$'\n'
     ARGS=''
     while [ "$1" ]; do ARGS="$ARGS$1 $2 $3$nl"; shift 3; done
     export ARGS
@@ -16,7 +14,7 @@ shift
 
 case $action in
     deploy_challenge|clean_challenge)
-        group_args
+        group_args "$@"
         "$(dirname "$0")"/${action/_/-}/mythic-dns01
         ;;
 esac
